d940abe6bf99a1f734540b87dc7d73c356d26f98
CxCore <91425802+CxCore-xyz@users.noreply.github.com>
2025-01-22 21:21:27 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -9,10 +9,12 @@ local Translations = {
     },
     success = {
         ["success"] = "You successfully refueled vehicle!",
+        ["jerrycan"] = "You successfully purchased jerrycan!",
     },
     info = {
         ["invehicle"] = "You can't be inside any vehicle while refueling!",
         ["progress_bar"] = "Refueling Vehicle",
+        ["progress_buyjerry"] = "Purchasing Jerrycan",
     },
     target = {
         ["buy_can"] = "Buy Jerrycan For $",
