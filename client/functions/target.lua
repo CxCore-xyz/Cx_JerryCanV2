0ba948c6503fec13b54fcdeb1a31b0e906770dcf
CxCore <91425802+CxCore-xyz@users.noreply.github.com>
2025-03-08 20:38:05 +0200

    Update target.lua

--- client/functions/target.lua
+++ client/functions/target.lua
@@ -1,5 +1,5 @@
 CreateThread(function() 
-   if Config.TargetPurchase then exports[Config.TargetScript]:AddTargetModel(Config.GasPumpModels, { 
+   if Config.UseTargetSystem then exports[Config.TargetScript]:AddTargetModel(Config.GasPumpModels, { 
       options = { 
           { 
              type = Config.TargetType, 
@@ -8,7 +8,12 @@ CreateThread(function()
              label = Lang:t("target.buy_can") ..Config.GasCanPrice 
           } 
           if Config.UseRefillSystem then
-                     
+          { 
+             --type = Config.RefillTargetType, 
+             --event = Config.RefillTargetTrigger, 
+             --icon =  Config.RefillTargetIcon, 
+             --label = Lang:t("target.refill_can") ..Config.GasRefillPrice 
+          }                      
           end
       }, 
       distance = 2.5 
